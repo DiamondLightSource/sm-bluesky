93d5f4b8548d417fe3f9031bc8ecb9d44791292a
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-06-16 13:33:47 +0000

    add auto centre diff

--- src/sm_bluesky/beamlines/i10/plans/centre_direct_beam.py
+++ src/sm_bluesky/beamlines/i10/plans/centre_direct_beam.py
@@ -1,11 +1,8 @@
 from collections.abc import Hashable
 
 import bluesky.plan_stubs as bps
-from dodal.beamlines.i10 import (
-    diffractometer,
-    sample_stage,
-)
-from dodal.common.types import MsgGenerator
+from bluesky.utils import MsgGenerator, plan
+from dodal.beamlines.i10 import diffractometer, focusing_mirror, sample_stage
 from ophyd_async.core import StandardReadable
 
 from sm_bluesky.beamlines.i10.configuration.default_setting import (
@@ -73,3 +70,57 @@ def move_pin_origin(wait: bool = True, group: Hashable | None = None) -> MsgGene
     yield from bps.abs_set(sample_stage().z, 0, wait=False, group=group)
     if wait:
         yield from bps.wait(group=group)
+
+
+@plan
+def beam_on_pin(
+    det: StandardReadable = RASOR_DEFAULT_DET,
+    det_name: str = RASOR_DEFAULT_DET_NAME_EXTENSION,
+    mirror_start: float = 4.8,
+    mirror_end: float = 5.2,
+    mirror_num: int = 100,
+    sy_start: float = -0.25,
+    sy_end: float = 0.25,
+    sy_num: int = 50,
+    pin_half_cut: float = 1.0,
+) -> MsgGenerator:
+    yield from bps.abs_set(sample_stage().y, pin_half_cut, wait=True)
+    yield from step_scan_and_move_fit(
+        det=det,
+        motor=focusing_mirror().fine_pitch,
+        start=mirror_start,
+        end=mirror_end,
+        num=mirror_num,
+        detname_suffix=det_name,
+        fitted_loc=StatPosition.MIN,
+    )
+    yield from step_scan_and_move_fit(
+        det=det,
+        motor=sample_stage().y,
+        start=sy_start,
+        end=sy_end,
+        num=sy_num,
+        detname_suffix=det_name,
+        fitted_loc=StatPosition.D_CEN,
+    )
+
+
+def beam_on_centre_diffractometer(
+    det: StandardReadable = RASOR_DEFAULT_DET,
+    det_name: str = RASOR_DEFAULT_DET_NAME_EXTENSION,
+) -> MsgGenerator:
+    """Move the pin hole to the centre of the beam."""
+    yield from move_pin_origin()
+    yield from bps.abs_set(sample_stage().y, -2.0, wait=True)
+    yield from centre_det_angles(det, det_name)
+    yield from beam_on_pin(det, det_name)
+    y_0 = yield from bps.rd(sample_stage().y)
+    yield from bps.abs_set(diffractometer().th, 180, wait=True)
+    yield from move_pin_origin()
+    y_180 = yield from bps.rd(sample_stage().y)
+    middle_y = (y_180 + y_0) / 2.0
+    while abs(middle_y - y_180) > 0.08:
+        yield from bps.rel_set(focusing_mirror().y, 0.001 * middle_y, wait=True)
+        yield from beam_on_pin(det, det_name)
+        y_180 = yield from bps.rd(sample_stage().y)
+    yield from bps.abs_set(diffractometer().th, 0, wait=True)
