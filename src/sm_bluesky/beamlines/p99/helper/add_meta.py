9121cced249550af3ff03f53fabdd7b99b9dd259
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-06-17 10:12:47 +0000

    add type hit to inner function

--- src/sm_bluesky/beamlines/p99/helper/add_meta.py
+++ src/sm_bluesky/beamlines/p99/helper/add_meta.py
@@ -2,6 +2,8 @@ from collections.abc import Callable
 from functools import wraps
 from typing import TypeVar, cast
 
+from blueapi.core import MsgGenerator
+
 TCallable = TypeVar("TCallable", bound=Callable)
 
 
@@ -13,7 +15,7 @@ def add_default_metadata(funcs: TCallable) -> TCallable:
     def inner(
         *args,
         **kwargs,
-    ):
+    ) -> MsgGenerator:
         if "md" in kwargs:
             kwargs["md"].update(P99_DEFAULT_METADATA)
         else:
