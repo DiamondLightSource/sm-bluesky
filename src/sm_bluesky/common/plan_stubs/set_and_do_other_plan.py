8bff8a469a4bb3348a50e0142cf0fc8d09a43924
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-07-23 15:32:45 +0000

    add typings

--- src/sm_bluesky/common/plan_stubs/set_and_do_other_plan.py
+++ src/sm_bluesky/common/plan_stubs/set_and_do_other_plan.py
@@ -1,30 +1,32 @@
-from typing import TypeVar
-
 import bluesky.plan_stubs as bps
 from bluesky import preprocessors as bpp
 from bluesky.plan_stubs import abs_set, sleep
-from bluesky.protocols import Movable
-from bluesky.utils import plan
+from bluesky.protocols import Movable, Readable
+from bluesky.utils import MsgGenerator, plan
 
-t = TypeVar("t", bound=Movable)
+MR = type("MR", (Movable, Readable), {"MR": "MR"})
 
 
-def set_setpoint_to_readback(set_signal, readback_signal):
+def set_setpoint_to_readback(set_signal, readback_signal) -> MsgGenerator:
     a = yield from bps.rd(readback_signal)
     yield from bps.abs_set(set_signal, a, wait=True)
 
 
 @plan
 def set_and_wait_within_tolerance(
-    set_signal,
-    value,
-    readback_signal,
-    tolerance,
-    plan=None,
-    final_plan=set_setpoint_to_readback,
+    set_signal: MR,
+    value: float,
+    tolerance: float,
+    readback_signal: Readable | None = None,
+    plan: MsgGenerator | None = None,
+    final_plan: MsgGenerator | None = None,
 ):
     if plan is None:
-        plan = sleep(0.1)
+        plan = sleep(1)
+    if readback_signal is None:
+        readback_signal = set_signal
+    if final_plan is None:
+        final_plan = set_setpoint_to_readback(set_signal, readback_signal)
     yield from abs_set(set_signal, value, wait=False)
 
     def inner_plan():
@@ -37,5 +39,5 @@ def set_and_wait_within_tolerance(
 
     yield from bpp.finalize_wrapper(
         plan=inner_plan(),
-        final_plan=final_plan(set_signal, readback_signal),
+        final_plan=final_plan,
     )
