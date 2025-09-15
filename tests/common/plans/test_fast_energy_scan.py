790de1bbf81a69f282eb63dc1015fe2376ffb757
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-09-15 13:07:50 +0000

    improve test

--- tests/common/plans/test_fast_energy_scan.py
+++ tests/common/plans/test_fast_energy_scan.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from unittest.mock import ANY
 
 import pytest
 from bluesky.run_engine import RunEngine
@@ -9,6 +10,7 @@ from dodal.devices.apple2_undulator import (
 from dodal.devices.i10.i10_apple2 import I10Apple2
 from dodal.devices.pgm import PGM
 from ophyd_async.core import (
+    Device,
     StrictEnum,
     init_devices,
 )
@@ -16,6 +18,7 @@ from ophyd_async.testing import assert_emitted, set_mock_value
 
 from sm_bluesky.common.plans import soft_fly_energy_scan
 
+from ...sim_devices.sim_stage import SimMotorExtra
 from ...test_data.common import (
     LOOKUP_TABLE_PATH,
 )
@@ -25,10 +28,16 @@ class Grating(StrictEnum):
     TESTING = "Grating"
 
 
+class FakePGM(Device):
+    def __init__(self, name=""):
+        self.energy = SimMotorExtra(instant=False)
+        super().__init__(name=name)
+
+
 @pytest.fixture
-async def mock_pgm(prefix: str = "BLXX-EA-DET-007:") -> PGM:
+async def mock_pgm(prefix: str = "BLXX-EA-DET-007:") -> FakePGM:
     async with init_devices(mock=True):
-        mock_pgm = PGM(prefix=prefix, grating=Grating, gratingPv="NLINES2")
+        mock_pgm = FakePGM()
     return mock_pgm
 
 
@@ -52,29 +61,52 @@ async def mock_energy(mock_pgm: PGM) -> EnergySetter:
     set_mock_value(mock_energy.id.id_jaw_phase.gate, UndulatorGateStatus.CLOSE)
     set_mock_value(mock_energy.id.id_jaw_phase.jaw_phase.velocity, 1)
     set_mock_value(mock_energy.id.gap.velocity, 2)
-    set_mock_value(mock_energy.id.gap.max_velocity, 2)
-    set_mock_value(mock_energy.id.gap.min_velocity, 0.01)
+    set_mock_value(mock_energy.id.gap.max_velocity, 200)
+    set_mock_value(mock_energy.id.gap.min_velocity, 0.0)
 
     set_mock_value(mock_energy.id.phase.btm_inner.velocity, 1)
     set_mock_value(mock_energy.id.phase.top_inner.velocity, 1)
     set_mock_value(mock_energy.id.phase.btm_outer.velocity, 1)
     set_mock_value(mock_energy.id.phase.top_outer.velocity, 1)
-    set_mock_value(mock_energy.pgm_ref().energy.acceleration_time, 1)
+    set_mock_value(mock_energy.pgm_ref().energy.acceleration_time, 0.1)
     set_mock_value(mock_energy.pgm_ref().energy.user_readback, 500)
     set_mock_value(mock_energy.pgm_ref().energy.user_setpoint, 500)
-    set_mock_value(mock_energy.pgm_ref().energy.max_velocity, 30)
+    set_mock_value(mock_energy.pgm_ref().energy.max_velocity, 50)
     set_mock_value(mock_energy.pgm_ref().energy.high_limit_travel, 1700)
     set_mock_value(mock_energy.pgm_ref().energy.low_limit_travel, 400)
     return mock_energy
 
 
-def test_soft_fly_energy_scan_success(mock_energy: EnergySetter, RE: RunEngine, det):
+async def test_soft_fly_energy_scan_success(
+    mock_energy: EnergySetter, RE: RunEngine, det
+):
     docs = defaultdict(list)
     det.start_simulation()
 
     def capture_emitted(name, doc):
         docs[name].append(doc)
 
-    RE(soft_fly_energy_scan([det], mock_energy, 700, 800, 0.2, 0.2), capture_emitted)
-    assert_emitted(docs, start=1, descriptor=1, event=1, stop=1)
-    # assert docs["event"][0]["data"] == ""
+    RE(
+        soft_fly_energy_scan([det], mock_energy, 700, 800, 0.2, 1e-3),
+        capture_emitted,
+        wait=True,
+    )
+
+    assert_emitted(docs, start=1, descriptor=1, event=ANY, stop=1)
+    # Number of event depend how fast motor is moving, it has to be more than 1
+    assert len(docs["event"]) > 1
+    # check the starting point
+    assert docs["event"][0]["data"] == {
+        "rand": ANY,
+        "mock_energy-id-energy": 750,
+        "mock_pgm-energy": 700.0,
+    }
+    # check end point
+    assert docs["event"][-1]["data"] == {
+        "rand": ANY,
+        "mock_energy-id-energy": 750,
+        "mock_pgm-energy": 810.0,
+    }
+    # speed reset
+    assert await mock_energy.pgm_ref().energy.velocity.get_value() == 1.0
+    assert await mock_energy.id.gap.velocity.get_value() == 2.0
