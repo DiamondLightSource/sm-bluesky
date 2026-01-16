9752d1059dc892fd91554e71115dbdf3c6b554d8
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-01-16 13:19:20 +0000

    update to latest version

--- src/sm_bluesky/beamlines/p99/plans/sample_stage_scanning.py
+++ src/sm_bluesky/beamlines/p99/plans/sample_stage_scanning.py
@@ -1,23 +1,18 @@
-import asyncio
-import math as mt
+from math import ceil, floor
 
 import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
-from aioca import caput
-from bluesky.utils import MsgGenerator
-from dodal.common.coordination import inject
+from bluesky.utils import MsgGenerator, plan
 from dodal.plan_stubs.data_session import attach_data_session_metadata_decorator
 from ophyd_async.core import (
     DetectorTrigger,
     StandardFlyer,
     TriggerInfo,
-    wait_for_value,
 )
-from ophyd_async.epics.pmac import (
-    Pmac,
-    PmacMotor,
-    PmacTrajectoryTriggerLogic,
-    PmacTrajInfo,
+from ophyd_async.epics.motor import Motor
+from ophyd_async.epics.pmac import PmacIO
+from ophyd_async.epics.pmac._pmac_trajectory import (
+    PmacTrajectoryTriggerLogic,  # noqa: PLC2701
 )
 from ophyd_async.fastcs.panda import (
     HDFPanda,
@@ -25,49 +20,9 @@ from ophyd_async.fastcs.panda import (
     PcompInfo,
     StaticPcompTriggerLogic,
 )
-from ophyd_async.fastcs.panda._block import PcompBlock
-from ophyd_async.plan_stubs import ensure_connected
-from scanspec.specs import Line, fly
+from scanspec.specs import Fly, Line
 
 X_MOTOR_RESOLUTION = -2 / 100000
-
-
-class _StaticPcompTriggerLogic(StaticPcompTriggerLogic):
-    """For controlling the PandA `PcompBlock` when flyscanning."""
-
-    def __init__(self, pcomp: PcompBlock) -> None:
-        self.pcomp = pcomp
-
-    async def kickoff(self) -> None:
-        await wait_for_value(self.pcomp.active, True, timeout=1)
-
-    async def prepare(self, value: PcompInfo) -> None:
-        await caput("BL99P-MO-PANDA-01:SRGATE1:FORCE_RST", "1", wait=True)
-        await asyncio.gather(
-            self.pcomp.start.set(value.start_postion),
-            self.pcomp.width.set(value.pulse_width),
-            self.pcomp.step.set(value.rising_edge_step),
-            self.pcomp.pulses.set(value.number_of_pulses),
-            self.pcomp.dir.set(value.direction),
-        )
-
-    async def stop(self):
-        pass
-
-
-def get_pcomp_info(width, start_pos, direction_of_sweep: PandaPcompDirection, num):
-    start_pos_pcomp = mt.floor(start_pos / X_MOTOR_RESOLUTION)
-    rising_edge_step = mt.ceil(abs(width / X_MOTOR_RESOLUTION))
-
-    panda_pcomp_info = PcompInfo(
-        start_postion=start_pos_pcomp,
-        pulse_width=1,
-        rising_edge_step=rising_edge_step,
-        number_of_pulses=num,
-        direction=direction_of_sweep,
-    )
-
-    return panda_pcomp_info
 
 
 def calculate_stuff(start, stop, num):
@@ -81,7 +36,26 @@ def calculate_stuff(start, stop, num):
     return width, start, stop, direction_of_sweep
 
 
+def get_pcomp_info(width, start_pos, direction_of_sweep: PandaPcompDirection, num):
+    start_pos_pcomp = floor(start_pos / X_MOTOR_RESOLUTION)
+    rising_edge_step = ceil(abs(width / X_MOTOR_RESOLUTION))
+
+    panda_pcomp_info = PcompInfo(
+        start_postion=start_pos_pcomp,
+        pulse_width=1,
+        rising_edge_step=rising_edge_step,
+        number_of_pulses=num,
+        direction=direction_of_sweep,
+    )
+
+    return panda_pcomp_info
+
+
+@plan
 def trajectory_fly_scan(
+    motor_fast: Motor,
+    motor_slow: Motor,
+    pmac: PmacIO,
     fast_start: float,
     fast_stop: float,
     fast_num: int,
@@ -89,30 +63,20 @@ def trajectory_fly_scan(
     slow_stop: float,
     slow_num: int,
     duration: float,
-    panda: HDFPanda = inject("panda"),  # noqa: B008
+    panda: HDFPanda,  # noqa: B008
 ) -> MsgGenerator:
-    panda_pcomp1 = StandardFlyer(_StaticPcompTriggerLogic(panda.pcomp[1]))
-    panda_pcomp2 = StandardFlyer(_StaticPcompTriggerLogic(panda.pcomp[2]))
-
-    pmac = Pmac(prefix="BL99P-MO-STEP-01", name="pmac")
-    motor_x = PmacMotor(prefix="BL99P-MO-STAGE-02:X", name="X")
-    motor_y = PmacMotor(prefix="BL99P-MO-STAGE-02:Y", name="Y")
-    yield from ensure_connected(pmac, motor_x, motor_y)
-
-    # spec = fly(
-    #     Repeat(number_of_sweeps, gap=True) * ~Line(motor_x, start, stop, num),
-    #     duration,
-    # )
-    spec = fly(
-        Line(motor_y, slow_start, slow_stop, slow_num)
-        * ~Line(motor_x, fast_start, fast_stop, fast_num),
-        duration,
+    panda_pcomp1 = StandardFlyer(StaticPcompTriggerLogic(panda.pcomp[1]))
+    panda_pcomp2 = StandardFlyer(StaticPcompTriggerLogic(panda.pcomp[2]))
+    spec = Fly(
+        duration
+        @ (
+            Line(motor_slow, slow_start, slow_stop, slow_num)  # type: ignore
+            * ~Line(motor_slow, fast_start, fast_stop, fast_num)  # type: ignore
+        )  # type: ignore
     )
 
-    info = PmacTrajInfo(spec=spec)
-
-    traj = PmacTrajectoryTriggerLogic(pmac)
-    traj_flyer = StandardFlyer(traj)
+    pmac_trajectory = PmacTrajectoryTriggerLogic(pmac)
+    pmac_trajectory_flyer = StandardFlyer(pmac_trajectory)
 
     @attach_data_session_metadata_decorator()
     @bpp.run_decorator()
@@ -121,14 +85,12 @@ def trajectory_fly_scan(
         width, _, _, direction_of_sweep = calculate_stuff(
             fast_start, fast_stop, fast_num
         )
-
         dir1 = direction_of_sweep
         dir2 = (
             PandaPcompDirection.POSITIVE
             if direction_of_sweep == PandaPcompDirection.NEGATIVE
             else PandaPcompDirection.NEGATIVE
         )
-
         pcomp_info1 = get_pcomp_info(width, fast_start, dir1, fast_num)
         pcomp_info2 = get_pcomp_info(width, fast_stop, dir2, fast_num)
 
@@ -138,8 +100,7 @@ def trajectory_fly_scan(
             livetime=duration,
             deadtime=1e-5,
         )
-
-        yield from bps.prepare(traj_flyer, info, wait=True)
+        yield from bps.prepare(pmac_trajectory_flyer, spec, wait=True)
         # prepare both pcomps
         yield from bps.prepare(panda_pcomp1, pcomp_info1, wait=True)
         yield from bps.prepare(panda_pcomp2, pcomp_info2, wait=True)
@@ -147,8 +108,8 @@ def trajectory_fly_scan(
         yield from bps.prepare(panda, panda_hdf_info, wait=True)
 
         yield from bps.kickoff(panda, wait=True)
-        yield from bps.kickoff(traj_flyer, wait=True)
+        yield from bps.kickoff(pmac_trajectory_flyer, wait=True)
 
-        yield from bps.complete_all(traj_flyer, panda, wait=True)
+        yield from bps.complete_all(pmac_trajectory_flyer, panda, wait=True)
 
     yield from inner_plan()
