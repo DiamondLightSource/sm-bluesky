b0fae5915f30d6faa426efcfbf43deba1e1f7e30
eir17846 <victor.rogalev@diamond.ac.uk>
2026-02-20 15:30:43 +0000

    pgm detect grating

--- src/sm_bluesky/beamlines/i05/plans/m1m3_alignment.py
+++ src/sm_bluesky/beamlines/i05/plans/m1m3_alignment.py
@@ -3,10 +3,11 @@ from bluesky.utils import MsgGenerator
 from dodal.beamlines.i05 import devices as i05_devices
 from dodal.common import inject
 from dodal.device_manager import DeviceManager
-from dodal.devices.beamlines.i05_shared import PolynomCompoundMotors
+from dodal.devices.beamlines.i05_shared import Grating, PolynomCompoundMotors
 from dodal.devices.common_mirror import XYZPiezoSwitchingMirror
-from dodal.devices.motors import Motor, XYZPitchYawRollStage
-from ophyd_async.core import StandardReadable
+from dodal.devices.motors import XYZPitchYawRollStage
+from dodal.devices.pgm import PlaneGratingMonochromator
+from ophyd_async.core import StandardReadable, StrictEnum
 
 from sm_bluesky.beamlines.i05.configuration.constants import (
     M3MJ6_PITCH_OFFSET_800,
@@ -50,7 +51,7 @@ def m1es_pitch_1600(
 i05_devices.include(alignment_devices)
 
 
-def align_mirrors_800(
+def map_m1_m3_mirrors_800(
     m1_start: float,
     m1_end: float,
     m1_num: int,
@@ -58,35 +59,32 @@ def align_mirrors_800(
     m3_end: float,
     m3_num: int,
     det: StandardReadable = inject("dj7current_new"),
-    m1_pitch: PolynomCompoundMotors = inject("m1es_pitch_800"),
-    m3_pitch: Motor = inject("m3mj6_switching_mirror.pitch"),
+    m3: XYZPiezoSwitchingMirror = inject("m3mj6_switching_mirror"),
 ) -> MsgGenerator:
     """
-    Plan to align the M1 and M3 mirrors by scanning the pitch of the mirror and moving
-    to the fitted position.
-
-    Parameters
-    ----------
-    det: StandardReadable,
-        Detector to be use for alignment.
-    motor: PolynomCompoundMotors
-        The compound motor that controls the pitch of the mirror.
-    start: float,
-        The starting position for the scan.
-    end: float,
-        The ending position for the scan.
-    num: int = 10,
-        The number of steps in the scan.
+    Plan to find optimal alignment of the M1.pitch and M3MJ6.pitch mirrors
     """
-
+    if get_pgm_grating() == Grating.PT_800:
+        m1_pitch = inject("m1es_pitch_800")
+    elif get_pgm_grating() == Grating.C_1600:
+        m1_pitch = inject("m1es_pitch_1600")
+    else:
+        raise ValueError("Unsupported grating for M1-M3 alignment.")
     yield from grid_scan(
         [det],
         m1_pitch,
         m1_start,
         m1_end,
         m1_num,
-        m3_pitch,
+        m3.pitch,
         m3_start,
         m3_end,
         m3_num,
     )
+
+
+async def get_pgm_grating(
+    pgm: PlaneGratingMonochromator = inject("pgm"),
+) -> StrictEnum:
+    reading = await pgm.grating.get_value()
+    return reading
