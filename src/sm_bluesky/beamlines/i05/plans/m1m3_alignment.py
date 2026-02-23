89048be4c2cad496b6e706ec140ce4a55858f025
eir17846 <victor.rogalev@diamond.ac.uk>
2026-02-23 11:38:35 +0000

    add docs

--- src/sm_bluesky/beamlines/i05/plans/m1m3_alignment.py
+++ src/sm_bluesky/beamlines/i05/plans/m1m3_alignment.py
@@ -1,5 +1,5 @@
 from bluesky.plans import grid_scan
-from bluesky.utils import MsgGenerator
+from bluesky.utils import MsgGenerator, plan
 from dodal.beamlines.i05 import devices as i05_devices
 from dodal.common import inject
 from dodal.device_manager import DeviceManager
@@ -10,40 +10,56 @@ from dodal.devices.pgm import PlaneGratingMonochromator
 from ophyd_async.core import StandardReadable, StrictEnum
 
 from sm_bluesky.beamlines.i05.configuration.constants import (
-    M3MJ6_PITCH_OFFSET_800,
-    M3MJ6_PITCH_OFFSET_1600,
-    M3MJ6_X_OFFSET_800,
-    M3MJ6_X_OFFSET_1600,
+    M3MJ6_X_POLY_800,
+    M3MJ6_X_POLY_1600,
 )
+
+# At this point in the alignment, we already have nice parameters for the dependence of
+# m3mj6_x, but we want to establish the dependence of m3mj6_pitch vs m1es_pitch.
+# The GDA scan command would be something like:
+# scan m1es_pitch -200 1600 75 m3mj6_pitch 4750 5350 8 waittime 0.1 dj7current_new
+# We then need to fit the peak at each value of m1es_pitch, and from the peak positions
+# we fit a third order polynomial. With m1es_pitch then well established, we
+# subsequently do the focusing scan using the gas cell.
+#
+# This scan actually takes over an hour, because the step motion of the hexapod
+# (m3mj6_pitch) is slow. In principle we could try to do it in a "fly" mode
+# with bluesky, which could be a significant speed up.
 
 # Define alignment devices and factory functions to create them.
 alignment_devices = DeviceManager()
 
 
+# This is a temp polynomial compound motor that maps the m1es_pitch to the already
+# defined m3mj6_x for finding m3 pitch dependence on m1es_pitch. Once we have the
+# m3mj6_pitch dependence on m1es_pitch, we can then define the real polynomial
+# compound motor for m3mj6_pitch and forget this temp one
 @alignment_devices.factory()
-def m1es_pitch_800(
+def m1_m3_x_800(
     m1_collimating_mirror: XYZPitchYawRollStage,
     m3mj6_switching_mirror: XYZPiezoSwitchingMirror,
 ) -> PolynomCompoundMotors:
     return PolynomCompoundMotors(
         m1_collimating_mirror.pitch,
         {
-            m3mj6_switching_mirror.x: M3MJ6_X_OFFSET_800,
-            m3mj6_switching_mirror.pitch: M3MJ6_PITCH_OFFSET_800,
+            m3mj6_switching_mirror.x: M3MJ6_X_POLY_800,
         },
     )
 
 
+# This is a temp polynomial compound motor that maps the m1es_pitch to the already
+# defined m3mj6_x for finding m3 pitch dependence on m1es_pitch. Once we have the
+# m3mj6_pitch dependence on m1es_pitch, we can then define the real polynomial
+# compound motor for m3mj6_pitch and forget this temp one
 @alignment_devices.factory()
-def m1es_pitch_1600(
+def m1_m3_x_1600(
     m1_collimating_mirror: XYZPitchYawRollStage,
     m3mj6_switching_mirror: XYZPiezoSwitchingMirror,
 ) -> PolynomCompoundMotors:
     return PolynomCompoundMotors(
         m1_collimating_mirror.pitch,
         {
-            m3mj6_switching_mirror.x: M3MJ6_X_OFFSET_1600,
-            m3mj6_switching_mirror.pitch: M3MJ6_PITCH_OFFSET_1600,
+            m3mj6_switching_mirror.x: M3MJ6_X_POLY_1600,
         },
     )
 
@@ -51,6 +67,13 @@ def m1es_pitch_1600(
 i05_devices.include(alignment_devices)
 
 
+# This will produce 2D map of m1es_pitch vs m3mj6_pitch, with the intensity of
+# dj7current_new as the value. We then fit the peak at each value of m1es_pitch,
+# and from the peak positions we fit a third order polynomial to find the dependence
+# of m3mj6_pitch on m1es_pitch. This will amend M3MJ6_PITCH_POLY_800/1600.
+# With m1es_pitch then well established,
+# we subsequently do the focusing scan using the gas cell.
+@plan
 def map_m1_m3_mirrors(
     m1_start: float,
     m1_end: float,
@@ -65,9 +88,9 @@ def map_m1_m3_mirrors(
     Plan to find optimal alignment of the M1.pitch and M3MJ6.pitch mirrors
     """
     if get_pgm_grating() == Grating.PT_800:
-        m1_pitch = inject("m1es_pitch_800")
+        m1_pitch = inject("m1_m3_x_800")
     elif get_pgm_grating() == Grating.C_1600:
-        m1_pitch = inject("m1es_pitch_1600")
+        m1_pitch = inject("m1_m3_x_1600")
     else:
         raise ValueError("Unsupported grating for M1-M3 alignment.")
     yield from grid_scan(
