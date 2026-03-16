7fde1562cb66af0f5275096478d6fc3bb99336f3
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-03-16 16:26:50 +0000

    add the skeleton class for test

---
+++ src/sm_bluesky/common/server/pulse_generator_shanghai tech.py
@@ -0,0 +1,48 @@
+from sm_bluesky.common.server import AbstractInstrumentServer
+
+
+class GeneratorServerShanghaiTech(AbstractInstrumentServer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.device = None  # Placeholder for your USB device connection
+
+    def connect_hardware(self) -> bool:
+        """Initialize the USB connection protocol."""
+        # TODO: Add your USB connection logic here
+        return True
+
+    def disconnect_hardware(self):
+        """Safely release the USB resource."""
+        # TODO: Add your USB disconnection logic here
+        pass
+
+    def _handle_command(self, cmd: bytes, arg: bytes) -> None:
+        """
+        Routes incoming commands to the pulse generator.
+        """
+        if cmd == b"disconnect":
+            self.disconnect_hardware()
+            pass
+
+        elif cmd == b"check_status":
+            # TODO: Logic to get hardware status
+            pass
+
+        elif cmd == b"set_delay":
+            # TODO: Logic to set delay using args
+            pass
+
+        elif cmd == b"get_delay":
+            # TODO: Return current delay
+            pass
+
+        elif cmd == b"reset_output_buffer":
+            # TODO: Logic to clear buffer
+            pass
+
+        elif cmd == b"pass_command":
+            # TODO: Forward raw command
+            pass
+
+        else:
+            pass
