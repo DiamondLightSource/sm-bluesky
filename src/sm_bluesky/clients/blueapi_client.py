a248377cc1944f28c9608f19b8e371e4bdda0fc4
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-04-23 08:15:03 +0000

    Addd name == main check

--- src/sm_bluesky/clients/blueapi_client.py
+++ src/sm_bluesky/clients/blueapi_client.py
@@ -9,43 +9,41 @@ from blueapi.client import BlueapiClient
 from blueapi.config import ApplicationConfig, HttpUrl, RestConfig, StompConfig, TcpUrl
 from blueapi.core.bluesky_types import DataEvent
 
-BEAMLINE = environ.get("BEAMLINE")
-if BEAMLINE is None:
-    raise RuntimeError("BEAMLINE environment variable not set.")
+if __name__ == "__main__":
+    BEAMLINE = environ.get("BEAMLINE")
+    if BEAMLINE is None:
+        raise RuntimeError("BEAMLINE environment variable not set.")
 
-
-print(f"Starting BlueAPI client for {BEAMLINE}.")
-bc = BlueapiClient.from_config(
-    ApplicationConfig(
-        api=RestConfig(url=HttpUrl(f"https://{BEAMLINE}-blueapi.diamond.ac.uk")),
-        stomp=StompConfig(
-            enabled=True,
-            url=TcpUrl(f"tcp://{BEAMLINE}-rabbitmq-daq.diamond.ac.uk:61613"),
-        ),
+    print(f"Starting BlueAPI client for {BEAMLINE}.")
+    bc = BlueapiClient.from_config(
+        ApplicationConfig(
+            api=RestConfig(url=HttpUrl(f"https://{BEAMLINE}-blueapi.diamond.ac.uk")),
+            stomp=StompConfig(
+                enabled=True,
+                url=TcpUrl(f"tcp://{BEAMLINE}-rabbitmq-daq.diamond.ac.uk:61613"),
+            ),
+        )
     )
-)
-print('Created BlueapiClient "bc" object.')
-print("Logging in...")
-bc.login()
+    print('Created BlueapiClient "bc" object.')
+    print("Logging in...")
+    bc.login()
 
+    def _feedback(evt):
+        match evt:
+            case DataEvent(name="start"):
+                print("Run started")
+            case DataEvent(name="stop", doc={"exit_status": status}):
+                print("Run complete: ", status)
+            case DataEvent(name="event", doc={"seq_num": point, "data": data}):
+                print(f"    Point {point}: {data}")
 
-def _feedback(evt):
-    match evt:
-        case DataEvent(name="start"):
-            print("Run started")
-        case DataEvent(name="stop", doc={"exit_status": status}):
-            print("Run complete: ", status)
-        case DataEvent(name="event", doc={"seq_num": point, "data": data}):
-            print(f"    Point {point}: {data}")
+    feedback_id = bc.add_callback(_feedback)
+    print("Installed feedback.")
 
+    print("\nGetting devices and plans...")
+    devs = bc.devices
+    plans = bc.plans
+    print(devs)
+    print(plans)
 
-feedback_id = bc.add_callback(_feedback)
-print("Installed feedback.")
-
-print("\nGetting devices and plans...")
-devs = bc.devices
-plans = bc.plans
-print(devs)
-print(plans)
-
-print("Please remember to configure the correct instrument session for bc.")
+    print("Please remember to configure the correct instrument session for bc.")
