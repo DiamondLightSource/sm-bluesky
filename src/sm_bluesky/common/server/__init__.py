eb87f654463bf590fe080cc9b39427418b80d0c0
Raymond Fan <raymond.fan@diamond.ac.uk>
2026-06-25 10:50:43 +0100

    284 refactor pulse generator severpy to inherit from abstractinstrumentserver (#285)
    * first crack at test
    
    * fixing typos
    
    * add start server
    
    * added stop
    
    * add  responses
    
    * add test for timeout
    
    * add _run_command_loop
    
    * rename command loop to serve_client
    
    * rename process lline
    
    * correct timeout and allow ipv6
    
    * added error testing and full cycle test
    
    * Add minimal docstring
    
    * add abc to class
    
    * correct test
    
    * remove redundancy
    
    * add the skeleton class for test
    
    * correct name and add connect_hardware test
    
    * add usb and connect_hardware
    
    * add connect and disconnect
    
    * add exception test
    
    * add command_registry for Abstract class commands.
    
    * merge abstract class update
    
    * added a _error_helper for logging and responds
    
    * add helper for error handling
    
    * clean up test
    
    * clean up test
    
    * make fixture for abstract_instrument_server test
    
    * clean up test
    
    * sync branch
    
    * add some test
    
    * change response to take byes
    
    * add passthrough and absracted hardware command and responds
    
    * added typing
    
    * add missing test
    
    * make use of error_helper
    
    * move to use readlin and added flush
    
    * remove try and catch as it is done by the base class
    
    * typo
    
    * add full commond flow test
    
    * remove comment
    
    * add strip to _send_hardware_command
    
    * add None to serial
    
    * add None to disconnect
    
    * add docs
    
    * complete test for connection error
    
    * move pyserial to optional dependencies
    
    * change args to a list
    
    * add contextmanager for hardware not responding
    
    * complete hardware_watch test
    
    * fix sigal required main thread.
    
    * cleanup test
    
    * change to use time_outcontext instead
    
    * add _check_timeout for sub class to check and raise timeout if it took too long
    
    * add docs
    
    * correct docs
    
    * rename docs
    
    * Log buffer reset in pulse generator
    
    move logging for buffer reset operation.
    
    * added new command "command_list" in the base class.
    
    * add test for send_command_list
    
    * move log to before sending device command
    
    * update uv
    
    * shorten line

--- src/sm_bluesky/common/server/__init__.py
+++ src/sm_bluesky/common/server/__init__.py
@@ -1,3 +1,4 @@
 from .abstract_instrument_server import AbstractInstrumentServer
+from .pulse_generator_shanghai_tech import GeneratorServerShanghaiTech
 
-__all__ = ["AbstractInstrumentServer"]
+__all__ = ["AbstractInstrumentServer", "GeneratorServerShanghaiTech"]
