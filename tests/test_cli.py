68114bf6483804ca5bb24586cd95fc9ceac4bfba
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-09-24 15:26:52 +0000

    fix main cli test

---
+++ tests/test_cli.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+from sm_bluesky import __version__
+
+
+def test_cli_version():
+    cmd = [sys.executable, "-m", "sm_bluesky", "--version"]
+    assert (
+        subprocess.check_output(cmd).decode().strip()
+        == f"sm-bluesky, version {__version__}"
+    )
